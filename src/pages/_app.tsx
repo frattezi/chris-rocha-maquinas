28267af577146afba40ebf3f838f48a002f22174
Pedro Frattezi Silva <pfrattezi@able.co>
2023-10-24 22:40:23 -0300

    Reconfigure base app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,11 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
 import { AppProps } from 'next/app'
-import PageLayout from '../components/PageLayout'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <PageLayout>
-        <Component {...pageProps} />
-      </PageLayout>
+      <Component {...pageProps} />
     </ChakraProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { Container } from '@chakra-ui/react'
 import BrandsCarousel from '../components/BrandsCarousel'
+import { NavBar } from '../components/NavBar'
+import WhatsAppWidget from '../components/WhatsAppWidget'
+import Banner from '../components/Banner'
 
 const Index = () => {
-  return <BrandsCarousel />
+  return (
+    <Container minW="100vw" p={0}>
+      <NavBar />
+      <Banner />
+      <WhatsAppWidget />
+      <BrandsCarousel />
+    </Container>
+  )
 }
 
 export default Index
